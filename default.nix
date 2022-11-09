e854e3ae3f38b03580de3a32201740c3255888c2
Yip Yew Mun <yipy0005@gmail.com>
2022-11-09 17:16:57 +0800

    adding an overlay to nix channel

--- default.nix
+++ default.nix
@@ -1,6 +1,15 @@
 let
-  # Import sources
-  sources = import ./nix/sources.nix;
+  sources = imports ./nix/sources.nix;
 
-# And return that specific nixpkgs
-in sources.nixpkgs
+  hello = pkgs.writeShellScriptBin "hello" ''
+    echo "Hello from the Nix channel overlay!"
+  '';
+
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (self: super: {
+        inherit hello;
+      })
+    ];
+  };
+in pkgs
